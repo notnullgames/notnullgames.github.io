b0fd6f317681c96947ff6839895b1afd7d7a4acd
David Konsumer <83857+konsumer@users.noreply.github.com>
2023-03-09 11:18:42 -0800

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,9 +1,9 @@
-const withNextra = require('nextra')({
+const withNextra = require('nextra')
+
+module.exports = withNextra({
   theme: 'nextra-theme-docs',
   themeConfig: './theme.config.tsx',
   images: {
     unoptimized: true
   }
 })
-
-module.exports = withNextra()
