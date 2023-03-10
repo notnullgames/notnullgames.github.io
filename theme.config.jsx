c6d0df3f352480426e3e4610695eb12de0f3ec40
David Konsumer <konsumer@jetboystudio.com>
2023-03-10 01:44:39 -0800

    flesh out the docs a bit

--- theme.config.jsx
+++ theme.config.jsx
@@ -8,6 +8,17 @@ export default {
   },
   docsRepositoryBase: 'https://github.com/notnullgames/notnullgames.github.io',
   footer: {
-    text: 'null0'
+    text: `${new Date().getFullYear()} © notnullgames`
+  },
+  banner: {
+    key: 'release-0.0.0',
+    text: <a href='https://github.com/notnullgames/null0/releases/tag/0.0.0' target='_blank' rel='noreferrer'>
+      🎉 Our first version was just released! It's just enough to make a cool 2D game. Read more →
+    </a>
+  },
+  useNextSeoProps () {
+    return {
+      titleTemplate: '%s – null0'
+    }
   }
 }
