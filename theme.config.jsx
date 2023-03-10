f937336fc2649bc515a728a8a5632a65946920a6
David Konsumer <konsumer@jetboystudio.com>
2023-03-09 21:36:40 -0800

    jsx

---
+++ theme.config.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import { DocsThemeConfig } from 'nextra-theme-docs'
+
+export default {
+  logo: <span>null0</span>,
+  project: {
+    link: 'https://github.com/notnullgames/null0'
+  },
+  docsRepositoryBase: 'https://github.com/notnullgames/notnullgames.github.io',
+  footer: {
+    text: 'null0'
+  }
+}
